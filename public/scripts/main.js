042a9b8b73c0f238e53e6382cb38120621548f69
Mario Steven Acevedo Mandujano <u20191e456@upc.edu.pe>
2022-07-06 16:06:25 -0500

    Initial Commit.

---
+++ public/scripts/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    alert("¡Suscripción elegida!");
+    top.location.href = "plans.html";
+}
+function sendEmail() {
+    alert("¡Correo enviado!");
+    top.location.href = "plans.html";
+}
